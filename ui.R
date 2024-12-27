581a187479e576877efa2cbffa76660df8759f62
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-27 15:44:40 -0600

    customized to us

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 # Define UI
 ui <- navbarPage(
-  title = "库存管理系统（国内端）",
+  title = "库存管理系统（美国端）",
   theme = shinytheme("flatly"), # 可选主题
   position = "fixed-top",
   
@@ -126,100 +126,6 @@ ui <- navbarPage(
   ),
   
   tabPanel(
-    "采购登记", icon = icon("shopping-cart"),
-    div(
-      class = "layout-container",  # Flexbox 容器
-      div(
-        class = "sticky-sidebar",  # sticky 侧边栏
-        
-        fluidRow(
-          column(10, 
-                 selectizeInput("new_maker", "供应商:", choices = NULL, width = "100%",
-                                options = list(placeholder = '输入供应商名称（或拼音）进行搜索', maxOptions = 500))
-          ),
-          column(2, 
-                 div(style = "display: flex; justify-content: flex-start; align-items: center; height: 100%;", 
-                     actionButton("add_supplier_btn", label = NULL, icon = icon("plus"), 
-                                  style = "font-size: 14px; width: 100%; height: 34px; padding: 0px; margin-top: 26px;")
-                 )
-          )
-        ),
-        
-        typeModuleUI("type_module"),
-        
-        fluidRow(
-          column(7, uiOutput("new_name_combo_box_ui")),  # 动态生成ComboBox
-          
-          column(5, dateInput(
-            inputId = "purchase_date",
-            label = "采购日期:",
-            value = Sys.Date(),  # 默认日期为今天
-            width = "100%"
-          ))
-        ),
-        
-        fluidRow(
-          column(4, numericInput("new_quantity", "数量:", value = 0, min = 0, step = 1)),
-          column(4, numericInput("new_product_cost", "单价:", value = 0, min = 0)),
-          column(4, numericInput("new_shipping_cost", "运费", value = 0, min = 0))
-        ),
-        fluidRow(
-          column(9,textInput("new_sku", "SKU(自动生成):", value = "", width = "100%")),
-          column(3,actionButton("reset_btn", "清空", icon = icon("snowplow"), class = "btn-danger", 
-                                style = "font-size: 14px; width: 100%; height: 45px; padding: 0px; margin-top: 26px;"))
-        ),
-        
-        imageModuleUI("image_purchase"),
-        
-        fluidRow(
-          column(12, style = "text-align: left;", actionButton("add_btn", "添加/更新采购货品信息", width = "100%", icon = icon("pen"), style = "background-color: #006400; color: white;")),
-        ),
-        
-        tags$hr(style = "margin: 5px 0; border: none;")
-      ),
-      
-      div(
-        class = "main-panel",
-        div(
-          div(
-            tags$span(icon("shopping-cart"), style = "margin-right: 5px;"),  # 使用 span 包裹图标
-            "采购箱", style = "font-size: 18px; font-weight: bold; color: #333; background-color: #c3d8fa; padding: 10px; text-align: center; border-radius: 4px;"
-          ),
-          column(12, DTOutput("added_items_table")),
-          
-          div(
-            style = "padding: 20px 0;",  # 添加上下20px的padding
-            fluidRow(
-              column(3, actionButton("delete_btn", "删除选中记录", icon = icon("trash"), class = "btn-danger")),
-              column(6, div(
-                textOutput("total_cost"),
-                style = "font-size: 20px; font-weight: bold; color: blue; text-align: center;"
-              )),
-              column(3, div(
-                style = "text-align: right;",
-                actionButton("confirm_btn", "确认登记采购货品", icon = icon("check"), class = "btn-primary")
-              ))
-            )
-          )
-        ),
-        
-        tags$hr(style = "margin: 20px 0; border: 1px solid #ddd;"),  # 添加分隔线
-        
-        div(
-          style = "display: flex; flex-direction: column;",
-          div(
-            style = "flex-grow: 1; overflow-y: auto; padding-top: 10px;",  # 表格自适应高度
-            div(
-              id = "item_table_container_purchase",
-              uniqueItemsTableUI("unique_items_table_purchase")
-            )
-          )
-        )
-      )
-    )
-  ), # end of 采购登记 tab
-  
-  tabPanel(
     "入库", icon = icon("arrow-circle-down"),
     div(
       class = "layout-container",  # Flexbox 容器
@@ -335,69 +241,6 @@ ui <- navbarPage(
       )
     )
   ), # end of 入库 tab
-  
-  tabPanel(
-    "出库", icon = icon("arrow-circle-up"),
-    div(
-      class = "layout-container",  # Flexbox 容器
-      div(
-        class = "sticky-sidebar",  # sticky 侧边栏
-        style = "width: 400px;", # override 宽度
-        
-        itemFilterUI(id = "outbound_filter", border_color = "#28A745", text_color = "#28A745"),
-        
-        tags$hr(), # 分隔线
-        
-        div(
-          class = "card",
-          style = "margin-bottom: 20px; padding: 20px; border: 1px solid #007BFF; border-radius: 8px; box-shadow: 0px 4px 6px rgba(0,0,0,0.1);",
-          tags$h4("出库操作", style = "color: #007BFF; font-weight: bold; margin-bottom: 15px;"),
-          textInput("outbound_sku", NULL, placeholder = "请扫描或输入SKU", width = "100%"),
-          
-          tags$div(
-            class = "card",
-            style = "padding: 15px; border: 2px solid #007BFF; border-radius: 8px; background-color: #f9f9f9; box-shadow: 0px 4px 6px rgba(0,0,0,0.1);",
-            tags$h4("选择国际运输方式:", style = "font-size: 18px; font-weight: bold; margin-bottom: 15px;"),
-            radioButtons(
-              inputId = "outbound_shipping_method",
-              label = NULL, # 将标签移到卡片标题
-              choices = list("空运" = "空运", "海运" = "海运"),
-              selected = "空运",  # 默认选择空运
-              inline = TRUE       # 设置为横向排布
-            )
-          ),
-          
-          actionButton(
-            "confirm_outbound_btn", 
-            "确认出库", 
-            icon = icon("check"), 
-            class = "btn-primary", 
-            style = "font-size: 16px; width: 100%; height: 42px; margin-top: 10px;"
-          )
-        )
-      ),
-      
-      div(
-        class = "main-panel",
-        div(
-          style = "height: 300px; margin-bottom: 10px;",
-          column(12, uiOutput("outbound_item_info"), style = "margin-bottom: 10px;") # 动态渲染物品信息
-        ), 
-        
-        div(
-          style = "display: flex; flex-direction: column;",
-          div(
-            style = "flex-grow: 1; overflow-y: auto; padding-top: 10px;",  # 表格自适应高度
-            div(
-              id = "item_table_container_outbound",
-              uniqueItemsTableUI("unique_items_table_outbound")
-            )
-          )
-        )
-      )
-    )
-  ), # end of 出库 tab
-  
   
   tabPanel(
     "售出", icon = icon("dollar-sign"),
