1f2bcd02815a4641cdbe636d522ee3ad33f1d0df
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-15 03:31:11 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -38,7 +38,17 @@ ui <- navbarPage(
         white-space: nowrap !important; /* 确保所有子元素在单行内 */
       }
       
-      /* 当屏幕宽度小于 1380px 时，隐藏标题 */
+      /* 鼠标悬停时修改标题颜色 */
+      .navbar-brand:hover {
+        color: #FFD700 !important; /* 悬停时标题文字颜色 */
+      } 
+      /* 鼠标悬停导航项时的颜色 */
+      .navbar-nav > li > a:hover {
+        color: #FFD700 !important;           /* 悬停文字颜色 */
+        background-color: #4b0363 !important;/* 悬停背景颜色 */
+      }
+    
+     /* 当屏幕宽度小于 1380px 时，隐藏标题 */
       @media (max-width: 1380px) {
         .navbar-brand {
           display: none !important;
