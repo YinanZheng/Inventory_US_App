823563282b8924421070f82fdfa29d160c8dbb1b
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-29 09:18:50 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -238,7 +238,7 @@ ui <- navbarPage(
         
          // 入库成功音效
         function playInboundSuccessSound() {
-          var audio = new Audio('http://54.254.120.88/sounds/inbound_success.mp3');
+          var audio = new Audio('http://54.254.120.88/sounds/success-josie.mp3');
           audio.play();
         }
         
